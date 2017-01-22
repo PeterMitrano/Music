bb8a31496277fb04e77265499008795e21c29e24
Peter Mitrano <mitranopeter@gmail.com>
2017-01-22 00:05:13 -0500

    still not getting what I want for random notes in chords

--- generate_notes.py
+++ generate_notes.py
@@ -1,5 +1,6 @@
 from numpy import random
 from music21 import midi
+from music21.midi import translate
 
 mt = midi.MidiTrack(1)
 
@@ -50,26 +51,92 @@ def populate_midi_track_from_data(mt, data):
 
     return mt
 
+# describe the chord progression
+chords = [
+    {'beats': 4, 'pitches': [35, 39, 42, 46,
+                             47, 51, 54, 58,
+                             59, 63, 66, 70,
+                             71, 75, 78, 82
+                             ]}, # B7
+    {'beats': 4, 'pitches': [34, 37, 41, 44,
+                             46, 49, 53, 56,
+                             58, 61, 65, 68,
+                             70, 73, 77, 80
+                             ]}, # B flat minor 7 / D flat
+    {'beats': 4, 'pitches': [32, 35, 39, 42,
+                             44, 47, 41, 44,
+                             56, 59, 63, 66,
+                             68, 71, 75, 78,
+                             80, 83, 87, 90
+                             ]}, # A flat minor 7
+    {'beats': 2, 'pitches': [25, 29, 32,
+                             37, 41, 44,
+                             49, 53, 55,
+                             61, 65, 68,
+                             73, 77, 80,
+                             85, 89, 92
+                             ]}, # D flat major
+    {'beats': 2, 'pitches': [24, 27, 33,
+                             36, 39, 45,
+                             48, 51, 57,
+                             60, 63, 69,
+                             72, 75, 81,
+                             84, 87, 93
+                             ]}, # E flat diminished 7 ??? not sure
+]
 
 # duration, pitch, velocity
-data = []
-for i in range(1024): # should be 1024
+data = [[1024, 59, 100]] # one start note
+total_duration = 0
+
+num_beats = 512
+chord_idx = 0
+beat_idx = 0
+
+# note array is ordered [duration, pitch, velocity]
+for i in range(num_beats):
+
+    # generate one beat
     note = [1024, 0, 0]
+    current_chord = chords[chord_idx]
+
     e = random.random()
-    if e < 0.75:
-        # new note
-        duration = abs(int(random.randn() + 2))
-        note[0] = duration * 1024
-        note[1] = random.randint(30,90)
-        note[2] = random.randint(20, 127)
-        data.append(note)
-    else:
+    last_note_was_rest = data[-1][2] < 0
+    if e < 0.15:
         # rest
+        note[0] = 256
+        note[1] = 0
         note[2] = 0
         data.append(note)
+    elif last_note_was_rest and e < 0.25:
+        # sustain rest
+        data[-1][0] += 256
+    elif not last_note_was_rest and e < 0.75:
+        # sustain note
+        data[-1][0] += 256
+    else:
+        # new note
+        note[0] = 256  # 256 is 16th note
 
+        if random.random() < 0.00:  # totally random note
+            note[1] = random.randint(30, 90)
+        else:
+            # pick note from chord
+            rand_idx = random.randint(0, len(current_chord['pitches']))
+            pitch = current_chord['pitches'][rand_idx]
+            note[1] = pitch
+
+        note[2] = random.randint(20, 127)
+        data.append(note)
+
+    # change chords
+    beat_idx += 1
+    if beat_idx == current_chord['beats']:
+        beat_idx = 0
+        chord_idx = (chord_idx + 1) % len(chords)
+
+print(data)
 populate_midi_track_from_data(mt, data)
-print(mt)
 
 mf = midi.MidiFile()
 mf.ticksPerQuarterNote = 1024 #  magic number?
@@ -78,3 +145,7 @@ mf.tracks.append(mt)
 mf.open('comp1 Project/out.mid', 'wb')
 mf.write()
 mf.close()
+
+# translate and show
+# s = translate.midiFileToStream(mf)
+# s.show()
